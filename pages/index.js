a6506a383fccad8cadcefa0565a893abf980bc3e
Anthony <anthonym@scorebooklive.com>
2023-04-12 13:32:33 -0600

    update site intro

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,10 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Hi, I'm Anthony</p>
+        <br />
         <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          I am practicing Next.js with this blog site.
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
